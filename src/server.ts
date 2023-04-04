353b4ee629937d216182e81fb174c0000c5d9f4a
JonasRocha189 <jonas.rocha189@gmail.com>
2023-04-04 16:28:45 -0300

    Routes and middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,39 @@
 import fastify from 'fastify'
-import { knex } from './database'
+// import { knex } from './database'
 // import crypto from 'node:crypto'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  // Rota para testar o Knex
-  // const tables = await knex('sqlite_schema').select('*')
-  // return tables
+app.register(cookie)
 
-  // Query para inserir dados
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: 1000,
-  //   })
-  //   .returning('*')
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
+
+/* 
+** Rota para testar o Knex
+app.get('/hello', async () => {
+const tables = await knex('sqlite_schema').select('*')
+return tables
+
+Query para inserir dados
+const transaction = await knex('transactions')
+  .insert({
+    id: crypto.randomUUID(),
+    title: 'Transação de teste',
+    amount: 1000,
+  })
+  .returning('*')
 
   const transaction = await knex('transactions')
     .where('amount', 1000)
     .select('*')
 
   return transaction
-})
+}) */
 
 app
   .listen({
