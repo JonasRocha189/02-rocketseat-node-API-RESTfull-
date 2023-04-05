bb9c3a3de68c4da66dcb365e8d4d2c5954ff5a6b
JonasRocha189 <jonas.rocha189@gmail.com>
2023-04-05 12:27:19 -0300

    automated tests

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,5 @@
-import fastify from 'fastify'
-// import { knex } from './database'
-// import crypto from 'node:crypto'
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
-
-const app = fastify()
-
-app.register(cookie)
-
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
-
-/* 
-** Rota para testar o Knex
-app.get('/hello', async () => {
-const tables = await knex('sqlite_schema').select('*')
-return tables
-
-Query para inserir dados
-const transaction = await knex('transactions')
-  .insert({
-    id: crypto.randomUUID(),
-    title: 'Transação de teste',
-    amount: 1000,
-  })
-  .returning('*')
-
-  const transaction = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transaction
-}) */
 
 app
   .listen({
---
+++ test/transactions.spec.ts
@@ -0,0 +1,125 @@
+import {expect,  beforeAll, afterAll, describe, it, beforeEach} from 'vitest'
+import { execSync } from 'node:child_process'
+import { app } from '../src/app'
+import request from 'supertest'
+
+describe('Transactions routes',() => {
+
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit'
+    }).expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server).post('/transactions').send({
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit'
+    })
+
+    const cookie = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New Transaction",
+        amount: 5000
+      })
+    ])
+
+  })
+
+
+  it('should be able to get a specifc transaction', async () => {
+    const createTransactionResponse = await request(app.server).post('/transactions').send({
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit'
+    })
+
+    const cookie = createTransactionResponse.get('Set-Cookie')
+
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie)
+      .expect(200)
+    
+    
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+    
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookie)
+      .expect(200)
+  
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: "New Transaction",
+        amount: 5000
+      })
+    )
+  })
+
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    })
+  })
+})
+
+// Primeiro teste
+// test('o usuário consegue criar uma nova transação', () => {
+//   // fazer a chamada HTTP para criar uma nova transação
+//   const responseStatusCode = 201
+
+//   // validação
+//   expect(responseStatusCode).toEqual(201)
+// })
+
